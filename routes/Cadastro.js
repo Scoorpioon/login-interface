bae9c5eb1e626e276dd312cb7cf352a35bcd5018
Gabriel Alexandre <104833986+Scoorpioon@users.noreply.github.com>
2023-06-21 22:31:56 -0300

    funções e ajuste de insert
    Implementado função de capitalize e ajustado dados a serem inseridos no banco de dados para uma melhor leitura.

--- routes/Cadastro.js
+++ routes/Cadastro.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const Usuarios = require('../models/Usuarios')
+
+const capitalize = (palavra) => {
+    const stringCapitalizada = palavra.charAt(0).toUpperCase() + palavra.slice(1);
+    return stringCapitalizada;
+};
 
 // Routes
     router.get('/', (req, res) => {
@@ -7,15 +13,19 @@ const router = express.Router();
     });
 
     router.post('/sucesso', (req, res) => {
-        Usuarios.create({
-            nome: req.body.nome,
-            idade: req.body.idade,
-            email: req.body.email,
-            senha:req.body.senha
-        }).then(() => {console.log('Deu certo!')}).catch((error) => {console.log('Erro: ') + error});
+        console.log(req.body)
 
         res.render('logado');
-    })
+        let nomeRecebido = capitalize(req.body.nome);
+        let emailRecebido = capitalize(req.body.email);
+
+        Usuarios.create({
+            nome: nomeRecebido,
+            idade: req.body.idade,
+            email: emailRecebido,
+            senha:req.body.senha
+        }).then(() => {console.log('Deu certo!')}).catch((error) => {console.log('Erro: ') + error});
+    });
 
 // Ending
-    module.exports = router;
+module.exports = router;
